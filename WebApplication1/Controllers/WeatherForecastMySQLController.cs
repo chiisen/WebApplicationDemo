26240051985511ef834e08b7a83fcaa52ded5ad9
chiisne <chiisen@hotmail.com>
2023-03-28 04:55:35 +0800

    feat: 初始化的 commit

---
+++ WebApplication1/Controllers/WeatherForecastMySQLController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+using System.Data;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastMySQLController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastMySQLController> _logger;
+
+        private readonly MySqlConnectionStringBuilder _builder = new()
+        {
+            Server = "127.0.0.1",
+            UserID = "dev",
+            Password = "123456",
+            Database = "dev",
+        };
+
+        private readonly string _key = "Summaries";
+
+        public WeatherForecastMySQLController(ILogger<WeatherForecastMySQLController> logger)
+        {
+            _logger = logger;
+
+            _logger.LogInformation("初始化 WeatherForecastMySQL");
+        }
+
+        [HttpGet(Name = "GetWeatherForecastMySQL")]
+        public IEnumerable<WeatherForecastMySQL> Get()
+        {
+            using var connection = new MySqlConnection(_builder.ConnectionString);
+            connection.Open();
+
+            using var command = connection.CreateCommand();
+            command.CommandText = @"SELECT * FROM WeatherForecast;";
+            /*
+            command.CommandText = @"SELECT * FROM WeatherForecast WHERE Summaries = @Summaries;";
+            command.Parameters.AddWithValue("@Summaries", "Cool");
+            */
+
+            // execute the command and read the results
+            using var reader = command.ExecuteReader();
+            List<string> Summaries_ = new();
+            while (reader.Read())
+            {
+                var Sum_ = reader.GetString(_key);
+                Summaries_.Add(Sum_);
+            }
+
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecastMySQL
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries_[Random.Shared.Next(Summaries_.Count)]
+            })
+            .ToArray();
+        }
+
+        /// <summary>
+        /// Redis 新增一筆資料
+        /// </summary>
+        /// <param name="json">新增的 json 格式資料</param>
+        /// <returns>回傳執行結果</returns>         
+        [HttpPost(Name = "PostWeatherForecastMySQL")]
+        public string Post(string json)
+        {
+            //在 WeatherForecast 資料表新增一筆資料
+            using var connection = new MySqlConnection(_builder.ConnectionString);
+            connection.Open();
+            string sql = $"INSERT INTO WeatherForecast ( `Summaries` ) VALUES ( '{json}')";
+            MySqlCommand cmd = new(sql, connection);
+            int n = cmd.ExecuteNonQuery();
+
+            return $"【{json}】 新增 {n} 筆資料成功";
+        }
+
+        /// <summary>
+        /// Redis 刪除一筆資料
+        /// </summary>
+        /// <param name="key">指定要刪除的 Redis Key</param>
+        /// <returns>回傳執行結果</returns>
+        [HttpDelete("{key}", Name = "DeleteWeatherForecastMySQL")]
+        public string Delete(string key)
+        {
+            //刪除 WeatherForecast 資料表中 Summaries 欄位值為指定的資料
+            string sql = $"DELETE FROM WeatherForecast WHERE Summaries='{key}'";
+            using var connection = new MySqlConnection(_builder.ConnectionString);
+            connection.Open();
+            MySqlCommand cmd = new(sql, connection);
+            int n = cmd.ExecuteNonQuery();
+
+            return $"【{key}】 成功刪除";
+        }
+
+        [HttpPut("{json}", Name = "PutWeatherForecastMySQL")]
+        public string Put(string json)
+        {
+            //將 WeatherForecast 資料表中修改 Summaries 欄位值為指定內容
+            string sql = $"UPDATE WeatherForecast SET Summaries='{json}' WHERE id='1'";
+            using var connection = new MySqlConnection(_builder.ConnectionString);
+            connection.Open();
+            MySqlCommand cmd = new(sql, connection);
+            int n = cmd.ExecuteNonQuery();
+
+            return $"【{json}】 更新 {n} 筆資料成功";
+        }
+    }
+}
---
+++ WebApplication1/Controllers/WeatherForecastRedisController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using System.Data;
+using System.Text.Json;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastRedisController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastRedisController> _logger;
+
+        private readonly ConnectionMultiplexer _conn = ConnectionMultiplexer.Connect("127.0.0.1:6379");
+        private readonly string _key = "Summaries";
+
+        public WeatherForecastRedisController(ILogger<WeatherForecastRedisController> logger)
+        {
+            _logger = logger;
+
+            _logger.LogInformation("初始化 GetWeatherForecastRedis");
+        }
+
+        /// <summary>
+        /// Redis 取得所有資料
+        /// </summary>
+        /// <param name="key">指定要刪除的 Redis Key</param>
+        /// <returns>回傳執行結果</returns>
+        [HttpGet(Name = "GetWeatherForecastRedis")]
+        public IEnumerable<WeatherForecastRedis>? Get()
+        {
+            IDatabase db_ = _conn.GetDatabase(0);
+            RedisValue redisValue_ = db_.StringGet("Summaries");
+            if(redisValue_.IsNullOrEmpty)
+            {
+                return null;
+            }
+            string jsonString_ = (string)redisValue_;
+            var Summary_ = JsonSerializer.Deserialize<string[]>(jsonString_);
+            if(Summary_ == null)
+            {
+                return null;
+            }
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecastRedis
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summary_[Random.Shared.Next(Summary_.Length)]
+            })
+            .ToArray();
+        }
+
+        /// <summary>
+        /// Redis 新增一筆資料
+        /// </summary>
+        /// <param name="json">新增的 json 格式資料</param>
+        /// <returns>回傳執行結果</returns>
+        /// <response code="200">新增成功</response>
+        /// <response code="400">新增失敗</response>          
+        [HttpPost(Name = "PostWeatherForecastRedis")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public string Post(string json)
+        {
+            IDatabase db_ = _conn.GetDatabase(0);
+            db_.StringSet(_key, json);
+
+            return $"【{json}】 新增成功";
+        }
+
+        /// <summary>
+        /// Redis 刪除一筆資料
+        /// </summary>
+        /// <param name="key">指定要刪除的 Redis Key</param>
+        /// <returns>回傳執行結果</returns>
+        [HttpDelete("{key}", Name = "DeleteWeatherForecastRedis")]
+        public string Delete(string key)
+        {
+            IDatabase db_ = _conn.GetDatabase(0);
+            db_.KeyDelete(key);
+            
+
+            return $"【{key}】 成功刪除";
+        }
+
+        [HttpPut("{json}", Name = "PutWeatherForecastRedis")]
+        public string Put(string json)
+        {
+            IDatabase db_ = _conn.GetDatabase(0);
+            db_.StringSet(_key, json);
+
+
+            return $"【{json}】 成功更新";
+        }
+    }
+}
