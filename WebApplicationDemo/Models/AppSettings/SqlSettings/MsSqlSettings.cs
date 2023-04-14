982d55d3cb83b09b21d507e00f6e0424f8c562e7
DESKTOP-HUDNBUM\sam <sam@super-royal.com>
2023-04-14 15:14:13 +0800

    fix: 補上 TrustServerCertificate 的設定說明

--- WebApplicationDemo/Models/AppSettings/SqlSettings/MsSqlSettings.cs
+++ WebApplicationDemo/Models/AppSettings/SqlSettings/MsSqlSettings.cs
@@ -4,6 +4,10 @@
     {
         public string? Database { get; set; }
 
+        /// <summary>
+        /// SQL Server 安裝時已預設支援加密連線，但預設用的 TLS 憑證是自我簽署憑證(Self-Signed Certificate)，故要勾選 Trust server
+        /// certificate，否則會因無法驗證憑證有效性出現錯誤 一般都要勾選(true)
+        /// </summary>
         public string? TrustServerCertificate { get; set; }
 
     }
